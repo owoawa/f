d214f3d8aa173a9eaa1aed216afed0b084025ac8
owoawa <l+fullstack@oy.rs>
2021-12-03 15:42:28 +0100

    phonebook

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -4,11 +4,15 @@ import connectionService from '../services/Connection'
 const Persons = ({persons, newFilter, setPersons}) => {
   
   const removeById = (person) => {
-    window.confirm(`Delete ${person.name}?`) ? connectionService.deletePerson(person.id).then() : console.log('no')
+    window.confirm(`Delete ${person.name}?`) ? connectionService.deletePerson(person.id).then(r => setPersons(persons.filter(x => x.id !== person.id))) : console.log('no')
+  
+    // console.log(persons)
   }
 
   return (
+   
     <div>
+        {console.log(persons)}
         {persons.map(p => {
         if (p.name.toLowerCase().includes(newFilter.toLowerCase())) {
           return <p key={p.id}>{p.name} {p.number} <button onClick={() => removeById(p)}> delete </button></p>
